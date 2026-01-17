702bf7016b7d0172978fb9eafdb72a3fcd66a37c
Tainan Rezende <tainan.rezende97@gmail.com>
2026-01-17 20:09:15 -0300

    config: add deploy workflow and fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,17 +20,19 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://api.xgateglobal.com',
+  url: 'https://Tainan-Rezende.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Tainan-Rezende', // Usually your GitHub org/user name.
+  projectName: 'doc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
